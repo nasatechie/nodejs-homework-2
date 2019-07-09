45c160b5b160120a0a2f05a00790c9178c578c34
Nagasai Pallapotu <nasa.techie@gmail.com>
2019-07-10 01:40:05 +0530

    added constructor, changed readdirsync to readdir

--- src/dirwatcher/dirwatcher.js
+++ src/dirwatcher/dirwatcher.js
@@ -1,45 +1,53 @@
 import fs from 'fs';
 import { EventEmitter } from 'events';
 
-export class DirWatcher extends EventEmitter {
-    watch(path, delay) {
+export class DirWatcher extends EventEmitter{
+    constructor(){
         let files = [];
-        let latestFiles = [];
-        setInterval(() => {
-            latestFiles = fs.readdirSync(path);
-            if (Array.isArray(files) && files.length) {
+        let new_files = [];
+        super();
+    }
+    watch(path, delay){
+        
+        setInterval(()=>{
+            fs.readdir(path, (err, data) => {
+                this.new_files = data;
+            if(Array.isArray(this.files) && this.files.length){
                 // For addition of files
-                let change = this.compare(files, latestFiles);
-                if (change) {
-                    this.emit('changed', change);
-                } else {
+                let changed_files = this.compare(this.files, this.new_files);
+                if(changed_files) {
+                    this.emit('changed', changed_files);
+                }else{
                     //For changes in any files.
-                    files.forEach((file, index) => {
-                        if ((Date.now() - fs.statSync(path + '/' + file).mtimeMs) < 1000) {
-                            this.emit('changed', latestFiles[index]);
+                    this.files.forEach((file, index) => {
+                        if((Date.now() - fs.statSync(path+'/'+file).mtimeMs) < 1000){
+                            this.emit('changed',this.new_files[index]);
                         }
                     });
                 }
             }
-            files = fs.readdirSync(path);
+            });
+            fs.readdir(path, (err, data) => {
+                this.files = data;
+            });
         }, delay);
     }
 
-    compare(filesArray, new_filesArray) {
+    compare(filesArray, new_filesArray){
         let element = '';
-        if (filesArray.length === new_filesArray.length) {
-            for (let i = 0; i < filesArray.length; i++) {
-                if (new_filesArray[i] !== filesArray[i]) {
+        if(filesArray.length === new_filesArray.length){
+            for(let i=0; i<filesArray.length; i++) {
+                if(new_filesArray[i] !== filesArray[i]){
                     element = new_filesArray[i];
                 }
             };
         }
-        else {
-            for (let i = 0; i < Math.max(filesArray.length, new_filesArray.length); i++) {
-                if (new_filesArray[i] !== filesArray[i]) {
-                    element = filesArray[i]
-                        ? filesArray[i]
-                        : new_filesArray[i];
+        else{
+            for(let i=0; i<Math.max(filesArray.length, new_filesArray.length); i++) {
+                if(new_filesArray[i] !== filesArray[i]){
+                    element = filesArray[i] 
+                    ? filesArray[i]
+                    : new_filesArray[i];
                 }
             };
         }
